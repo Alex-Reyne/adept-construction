eb55d0a9141d81558df2ac46fd4b4d659c27fcbd
Alex-Reyne <creativereyne@gmail.com>
2022-03-05 19:38:20 -0600

    Export Comopnents for easier import

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import { Home } from './Home/Home';
+import { Contact } from './Contact/Contact';
+import { About } from './About/About';
+import { Values } from './Values/Values';
+
+export { Home, Contact, About, Values };
