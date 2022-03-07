98d0f9a389a57de7dc9b856e76c52dbbdd26ed2d
Alex-Reyne <creativereyne@gmail.com>
2022-03-06 22:46:42 -0600

    organized home content

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -3,15 +3,20 @@ import styles from './Home.module.scss';
 export const Home = () => {
   return (
     <div id="home" className={styles.home__container}>
-      <div style={styles.brand}>
-        <h1>Adept Construction</h1>
-        <h2>Adept Construction</h2>
-        <a href="#about">
-          <button>About Us</button>
-        </a>
-        <a href="#contact">
-          <button>Contact</button>
-        </a>
+      <div className={styles.home__overlay}></div>
+      <div className={styles.home__content}>
+        <div className={styles.home__brand}>
+          <h1 className={styles.title}>Adept Construction</h1>
+          <h2 className={styles.subtitle}>Building homes for families</h2>
+        </div>
+        <div className={styles.home__buttons}>
+          <a href="#about">
+            <button>About Us</button>
+          </a>
+          <a id={styles.home__contact_button} href="#contact">
+            <button>Contact</button>
+          </a>
+        </div>
       </div>
     </div>
   );
