9cdf419e4599c6738bc24655800f3f1309affeb6
Alex-Reyne <creativereyne@gmail.com>
2022-03-11 23:25:27 -0600

    add about content

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,5 +1,38 @@
 import styles from './About.module.scss';
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div id="about" className={styles.about__container}>
+      <h1 className={styles.about__title}>Our Story</h1>
+      <div className={styles.about__stories}>
+        <div className={styles.about__story}>
+          <h2 className={styles.about__head}>Beginnings</h2>
+          <p className={styles.about__paragraph}>
+            lkjhfa jhdskfh aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa
+            jhdskfh aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa jhdskfh
+            aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa jhdskfh aksdhkj
+            kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkh
+          </p>
+        </div>
+        <div className={styles.about__story}>
+          <h2 className={styles.about__head}>Beginnings</h2>
+          <p className={styles.about__paragraph}>
+            lkjhfa jhdskfh aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa
+            jhdskfh aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa jhdskfh
+            aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa jhdskfh aksdhkj
+            kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkh
+          </p>
+        </div>
+        <div className={styles.about__story}>
+          <h2 className={styles.about__head}>Beginnings</h2>
+          <p className={styles.about__paragraph}>
+            lkjhfa jhdskfh aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa
+            jhdskfh aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa jhdskfh
+            aksdhkj kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkhlkjhfa jhdskfh aksdhkj
+            kjhdfkhkja sdhsdk kj hjkhfkj sdskdj kasjhsad hsadjkh
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
